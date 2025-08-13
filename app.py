6cc9294a766d795b134c6179f6aca0c5cbd34d83
Attila314dev <attila.nemeth-00@proton.me>
2025-08-13 11:42:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+@app.get("/health")
+def health():
+    return {"ok": True}
