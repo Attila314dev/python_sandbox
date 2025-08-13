838bae864baf09988635191dbfa7d44fd3a5825b
Attila314dev <attila.nemeth-00@proton.me>
2025-08-13 12:43:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
 from flask import Flask, render_template
+from sqlalchemy import create_engine, text
+
 app = Flask(__name__)
+
+# DB engine létrehozása env-ből
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 @app.get("/")
 def home():
@@ -7,4 +14,9 @@ def home():
 
 @app.get("/health")
 def health():
-    return {"ok": True}
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        return {"ok": True, "db": "connected"}
+    except Exception as e:
+        return {"ok": False, "error": str(e)}
